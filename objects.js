80cc2a071b98f82249fec5b3af6ab6243bb337b2
erin johnson <erin.nicole.johnson@gmail.com>
2017-10-14 10:36:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,14 +4,15 @@ var playlist = {
 };
 
 
-//The body of the function should add the song and artist as a 
+//The body of the function should add the song and artist as a
 //key-value pair to the playlist object. The function should return the whole playlist.
 function updatePlaylist(playlist, artist, title) {
+  playlist[artist] = title
   return
 }
 
-//The body of the function should delete the key-value pair 
+//The body of the function should delete the key-value pair
 // from the playlist and return the updated playlist.
 function removeFromPlaylist(playlist, artist) {
-  return 
+  return
 }
