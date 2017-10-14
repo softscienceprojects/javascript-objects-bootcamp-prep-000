4878e32dd70f59b86954a2f4ac54fc2b6aeac331
erin johnson <erin.nicole.johnson@gmail.com>
2017-10-14 10:34:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,11 @@ var playlist = {
   Britney: 'hit me baby one more time',
   ShaniaTwain: 'man I feel like a woman'
 };
+
+function updatePlaylist(playlist, artist, title) {
+  return
+}
+
+function removeFromPlaylist(playlist, artist) {
+  
+}
